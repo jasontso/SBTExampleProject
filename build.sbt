ae0df92c5c0784519b6ab13d11c1cc1e4411202a
Json Tso <jasontso@gmail.com>
2017-08-12 12:05:38 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "SBTExampleProject"
+
+version := "0.1"
+
+scalaVersion := "2.12.3"
+        
---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,8 @@
+package example
+
+object Main extends App {
+  val ages = Seq(42,34,64,25,96)
+  println(s"The oldest person is ${ages.max}")
+}
+
+
